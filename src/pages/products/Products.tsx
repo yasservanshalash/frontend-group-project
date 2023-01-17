3be8f8f82638583bee23af69ef3e81abbcd56d5d
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-17 22:07:02 +0100

    created second navbar

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ProductList from "../../components/products/productList/ProductList";
 
-const Products = () => {
-  return <ProductList />;
+const Products = ({userInput} : {userInput: string}) => {
+  return <ProductList userInput={userInput}/>;
 };
 
 export default Products;
