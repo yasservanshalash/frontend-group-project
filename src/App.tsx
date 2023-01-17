a6d03472bb43595c4343929eed7c8b0d10b51d65
Helen <helen.hagos@integrify.io>
2023-01-17 12:19:21 +0100

    added products route

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import "./App.css";
 import ProductList from "./components/products/productList/ProductList";
 import Home from "./pages/home/Home";
 import ProductDetails from "./pages/productDetails/ProductDetails";
+import Products from "./pages/products/Products";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="" element={<Home />}></Route>
-        <Route path="/products" element={<ProductList />}></Route>
+        <Route path="/products" element={<Products />}></Route>
         <Route path="/products/:id" element={<ProductDetails />}></Route>
       </Routes>
     </div>
--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,24 +1,49 @@
-import { ProductType } from "./../../types/types";
 import { createSlice } from "@reduxjs/toolkit";
- 
-type IntialState = {
-  cartData: ProductType[];
-};
- 
-const initialState: IntialState = {
-  cartData: [],
-};
- 
-const cartProductSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {
-    addCartProduct: (state, action) => {
-      state.cartData = action.payload;
-    },
-  },
-});
 
-export const cartProductSliceActions = cartProductSlice.actions;
-const cartProductReducer = cartProductSlice.reducer;
-export default cartProductReducer;
+import { ProductType } from "./../../types/types";
+
+// type IntialState = {
+//   cartList: ProductType[];
+// };
+
+// const initialState: IntialState = {
+//   cartList: [],
+// };
+
+// const cartProductSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     addTocart: (state, action) => {
+//       const index = state.cartList.findIndex(
+//         (cartItem) => cartItem.title === action.payload.title
+//       );
+//       if (index !== -1) {
+//         state.cartList[index].quantity += 1;
+//       }
+//       state.cartList.push(action.payload);
+//     },
+//     increaseItemQuantity: (state, action) => {
+//       const item = state.cartList.find((item) => item.id === action.payload.id);
+//       item?.quantity
+//     },
+//     decreaseItemQuantity: (state, action) => {
+//       const item = state.cartList.find((item) => item.id === action.payload.id);
+//       if (item.quantity === 1) {
+//         item?.quantity === 1;
+//       } else {
+//         item.quantity -= 1;
+//       }
+//     },
+//     removeCartItem: (state, action) => {
+//       const removeItem = state.cartList.filter(
+//         (item) => item.id !== action.payload
+//       );
+//       state.cartList = removeItem;
+//     },
+//   },
+// });
+
+// export const cartProductSliceActions = cartProductSlice.actions;
+// const cartProductReducer = cartProductSlice.reducer;
+// export default cartProductReducer;
