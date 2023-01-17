d07dc79003c356e33675f4033cebb31c0fb70b36
Helen <helen.hagos@integrify.io>
2023-01-17 10:52:25 +0100

    added ProductDetails

--- src/pages/productDetails/ProductDetails.tsx
+++ src/pages/productDetails/ProductDetails.tsx
@@ -5,13 +5,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
 import getProductDetailData from "../../redux/thunk/productDetail";
 import { productDetailSliceActions } from "../../redux/slices/productDetailSlice";
+import { ProductType } from "../../types/types";
 
 const ProductDetails = () => {
   const proDetail = useSelector(
     (state: RootState) => state.productDetails.productDetails
   );
-  const dispatch = useDispatch<AppDispatch>();
   const { id } = useParams();
+  const product = useSelector((state: RootState) => state.productList.products);
+  const detail = product.filter((item) => item.id === Number(id));
+  console.log(detail, "detail");
+  console.log(detail[0].title);
+  console.log(typeof id, "id type");
+  const dispatch = useDispatch<AppDispatch>();
+
   const url = `https://fakestoreapi.com/products/${id} `;
   console.log(url, "my url");
   console.log(proDetail, "proooo");
@@ -20,7 +27,12 @@ const ProductDetails = () => {
   }, [dispatch, url]);
   return (
     <div>
-      <p>Name:{proDetail[0].title}</p>
+      <p>
+        Name:
+        {detail.map((item: ProductType) => (
+          <p>ID:{item?.id}</p>
+        ))}
+      </p>
     </div>
   );
 };
