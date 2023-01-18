7cc44e557fb5b0e2cd09190d57ebb473653061bc
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-18 11:19:24 +0100

    removed store conflict

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import productReducer from "./slices/producttSlice";
 import productDetailReducer from "./slices/productDetailSlice";
-<<<<<<< HEAD
 import cartReducer from "./slices/cartSlice";
-=======
-import cartSliceReducer from "./slices/cartSlice";
->>>>>>> 376042a9ca4e74bf88af0ead8aa93fc2c6b940ee
 import favoriteReducer from "./slices/favoriteSlice";
 
 const store = configureStore({
@@ -14,14 +9,10 @@ const store = configureStore({
     productList: productReducer,
     cartList: cartReducer,
     productDetails: productDetailReducer,
-<<<<<<< HEAD
-    favoriteReducer: favoriteReducer,
-=======
     favoriteList: favoriteReducer,
->>>>>>> 376042a9ca4e74bf88af0ead8aa93fc2c6b940ee
   },
 });
+export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
