8c2d1bf5eb737a5c3f6f91165e66975d1ee77dd4
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-18 11:05:41 +0100

    removed wishlist conflicts

--- src/components/wishList/wishListPage/WishListPage.tsx
+++ src/components/wishList/wishListPage/WishListPage.tsx
@@ -1,30 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
-<<<<<<< HEAD
-import { RootState } from "../../../redux/store";
-=======
-
 import { RootState } from "../../../redux/store";
 import { ProductType } from "../../../types/types";
->>>>>>> 376042a9ca4e74bf88af0ead8aa93fc2c6b940ee
 import WishListItem from "../wishListItem/WishListItem";
 
 const WishListPage = () => {
-  const wishList = useSelector(
-<<<<<<< HEAD
-    (state: RootState) => state.favoriteReducer.favorite
-  );
+  const wishList = useSelector((state: RootState) => state.favoriteReducer.favorite);
   return (
     <div>
       {wishList.map((item) => (
-=======
-    (state: RootState) => state.favoriteList.favorite
-  );
-  console.log(wishList, "wishes");
-  return (
-    <div>
-      {wishList.map((item: ProductType) => (
->>>>>>> 376042a9ca4e74bf88af0ead8aa93fc2c6b940ee
         <WishListItem item={item} />
       ))}
     </div>
