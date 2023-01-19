6098cfac336e4206aeca47f079270acba4aef7d6
Muhammad <mirfan00730@gmail.com>
2023-01-19 19:08:58 +0100

    home page update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import Slider from "../../components/slider/Slider";
 import PopularCategories from "../../components/popularCategories/PopularCategories";
 import PopularBrands from "../../components/popularBrands/PopularBrands";
+import ProductContent from "../../components/productContent/ProductContent";
 const Home = () => {
   return (
     <>
       <Slider />
       <PopularCategories />
       <PopularBrands />
+      <ProductContent />
     </>
   );
 };
