c2e5860ab31430fb85da9dc3b8a07d5baa7c0d44
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-19 19:28:22 +0100

    commented home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,26 @@
-import Slider from "../../components/slider/Slider";
-import PopularCategories from "../../components/popularCategories/PopularCategories";
-import PopularBrands from "../../components/popularBrands/PopularBrands";
-import ProductContent from "../../components/productContent/ProductContent";
+// import Slider from "../../components/slider/Slider";
+// import PopularCategories from "../../components/popularCategories/PopularCategories";
+// import PopularBrands from "../../components/popularBrands/PopularBrands";
+// import ProductContent from "../../components/productContent/ProductContent";
+// const Home = () => {
+//   return (
+//     <>
+//       <Slider />
+//       <PopularCategories />
+//       <PopularBrands />
+//       <ProductContent />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+import React from 'react'
+
 const Home = () => {
   return (
-    <>
-      <Slider />
-      <PopularCategories />
-      <PopularBrands />
-      <ProductContent />
-    </>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
