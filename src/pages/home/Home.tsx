bc759d9d945701021995c1ac626b05ed09907397
Helen <helen.hagos@integrify.io>
2023-01-19 20:43:43 +0100

    commented out ProductContent

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,26 +1,16 @@
-// import Slider from "../../components/slider/Slider";
-// import PopularCategories from "../../components/popularCategories/PopularCategories";
-// import PopularBrands from "../../components/popularBrands/PopularBrands";
+import Slider from "../../components/slider/Slider";
+import PopularCategories from "../../components/popularCategories/PopularCategories";
+import PopularBrands from "../../components/popularBrands/PopularBrands";
 // import ProductContent from "../../components/productContent/ProductContent";
-// const Home = () => {
-//   return (
-//     <>
-//       <Slider />
-//       <PopularCategories />
-//       <PopularBrands />
-//       <ProductContent />
-//     </>
-//   );
-// };
-
-// export default Home;
-
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Slider />
+      <PopularCategories />
+      <PopularBrands />
+      {/* <ProductContent /> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
